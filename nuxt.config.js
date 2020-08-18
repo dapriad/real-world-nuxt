41b9fa714eff535f922d85fc57c538f90c35ad92
Danny <dapriad@gmail.com>
2020-08-18 20:50:44 +0200

    Move API calls to a Service, Create Vuex Event Module, Use Vuex in Event List and Use Vuex in Event Show

--- nuxt.config.js
+++ nuxt.config.js
@@ -64,7 +64,8 @@ export default {
    ** See https://nuxtjs.org/api/configuration-build/
    */
   build: {},
-  loading: {
-    color: '#39b982',
-  },
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: '#39b982' },
 }
